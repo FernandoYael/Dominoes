d5359507ad6a601fbddbfb3e3c877ed21569255e
Fernando <126305470+FernandoYael@users.noreply.github.com>
2023-03-13 19:33:08 +0000

    Please enter the commit message for your changes. Lines starting

---
+++ Domino/Program.cs
@@ -0,0 +1,16 @@
+﻿class Domino
+{
+    
+}
+
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Domino d1= new Domino (1,1);
+        Domino d2= new Domino (2,2);
+        int total=d1+d2;
+    }
+}
